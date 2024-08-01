3c2cb3930cef2d07dc11fe9b79dc6468864e0f5d
VamsiP23 <90874211+VamsiP23@users.noreply.github.com>
2024-08-01 16:23:05 -0500

    iOS app created

---
+++ frontend-ios/Yumm/Yumm/YummApp.swift
@@ -0,0 +1,17 @@
+//
+//  YummApp.swift
+//  Yumm
+//
+//  Created by Vamsi Putti on 8/1/24.
+//
+
+import SwiftUI
+
+@main
+struct YummApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
