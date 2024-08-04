4a183f9f59520aa2a162ed2174679db33b78598e
VamsiP23 <90874211+VamsiP23@users.noreply.github.com>
2024-08-04 14:30:15 -0500

    Intro Screen + Log In Screen Added

---
+++ frontend-ios/Yumm/Yumm/Constants/YummColors.swift
@@ -0,0 +1,17 @@
+//
+//  YummColors.swift
+//  Yumm
+//
+//  Created by Vamsi Putti on 8/4/24.
+//
+
+import Foundation
+import SwiftUI
+
+class YummColors {
+    static let defaultBlack = Color(red: 0.06666666666666667, green: 0.06274509803921569, blue: 0.06274509803921569)
+    static let darkPink = Color(red: 0.8705882352941177, green: 0.00784313725490196, blue: 0.4117647058823529)
+    static let regPink = Color(red: 0.9921568627450981, green: 0.28627450980392155, blue: 0.615686274509804)
+    static let lightPink = Color(red: 0.996078431372549, green: 0.6039215686274509, blue: 0.788235294117647)
+    static let cream = Color(red: 1, green: 0.9215686274509803, blue: 0.8392156862745098)
+}
