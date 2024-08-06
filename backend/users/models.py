ecad5ac4a3a5127729614ace1ae20255191cb388
Shreyas-Pattabi <spattabik@gmail.com>
2024-08-05 19:14:56 -0500

    updated models

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from filters.models import Cuisine, DietaryRestriction, PriceRange
 
 # Create your models here.
+
+
+class User(AbstractUser):
+    first_name = models.CharField(max_length=100, blank=False)
+    email = models.EmailField(blank = False, unique = True)
+    phone_number = models.CharField(max_length=15, unique=True, blank=False)
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
+    profile_picture = models.ImageField(upload_to='profile_pictures/')
+    bio = models.TextField(blank=True, null=True)
+
+class FoodPreference(models.Model):
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
+    cuisines = models.ManyToManyField(Cuisine)
+    diet = models.ManyToManyField(DietaryRestriction)
+    price = models.ForeignKey(PriceRange, on_delete=models.SET_DEFAULT)
+
